dc019e9fecdefd24ec23488a7aac9ece4f4ebed8
jackhu <jackhu.521.rose@gmail.com>
2023-10-29 11:53:35 +0800

    int 0x80

--- boot.asm
+++ boot.asm
@@ -5,17 +5,30 @@ GPU equ 0xb800
 
 xchg bx, bx ; 断点
 
-call addtion
+; 注册系统调用内中断
+mov word [0x80 * 4], print ; 段偏移
+mov word [0x80 * 4 + 2], 0 ; 段基址
+; ---
+
+int 0x80
+
 jmp $
 
-addtion:
+print:
+    push ax
+    push es
+    push di
+
     mov ax, GPU
     mov es, ax
     mov di, 0
+    mov byte [es:di], 'H'
 
-    mov byte es:[di], 'H'
-    mov byte es:[di + 2], 'u'
-    ret
-    
+
+    pop di
+    pop es
+    pop ax
+    ret 
+
 times 510 - ($-$$) db 0
 dw 0xaa55
